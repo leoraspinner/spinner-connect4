62317059962711898ce4f91af6bb63b6ab2353fb
Leora Spinner <leoraspinner@Leoras-MacBook-Air-2.local>
2025-02-26 13:39:02 -0500

    changed test

--- src/test/java/ConnectBoardTest.java
+++ src/test/java/ConnectBoardTest.java
@@ -4,17 +4,20 @@ import spinner.connect4.ConnectBoard;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ConnectBoardTest {
+public class ConnectBoardTest
+{
 
     private ConnectBoard board;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp()
+    {
         board = new ConnectBoard(7, 6); // Create a board of width 7 and height 6
     }
 
     @Test
-    void testInsert() {
+    void testInsert()
+    {
         ConnectBoard board = new ConnectBoard(7, 6);
         board.insert(3, 'R');
         board.insert(3, 'Y');
@@ -24,7 +27,8 @@ public class ConnectBoardTest {
     }
 
     @Test
-    public void testIsFull() {
+    public void testIsFull()
+    {
         // Initially, the column should not be full
         assertFalse(board.isFull(0));
 
@@ -38,47 +42,15 @@ public class ConnectBoardTest {
     }
 
     @Test
-    public void testCalculateWinner() {
-        // Test horizontal win
-        board.insert(0, 'R');
-        board.insert(1, 'R');
-        board.insert(2, 'R');
-        board.insert(3, 'R');
-        assertEquals('R', board.calculateWinner()); // Should return 'R'
+    public void calculateWinner()
+    {
+        ConnectBoard board = new ConnectBoard(7, 6);
+        board.insert(0, 'X');
+        board.insert(1, 'X');
+        board.insert(2, 'X');
+        board.insert(3, 'X');
 
-        // Reset the board
-        board = new ConnectBoard(7, 6);
-
-        // Test vertical win
-        for (int i = 0; i < 4; i++) {
-            board.insert(0, 'Y');
-        }
-        assertEquals('Y', board.calculateWinner()); // Should return 'Y'
-
-        // Reset the board
-        board = new ConnectBoard(7, 6);
-
-        // Test diagonal win (bottom-left to top-right)
-        board.insert(0, 'R');
-        board.insert(1, 'Y');
-        board.insert(1, 'R');
-        board.insert(2, 'Y');
-        board.insert(2, 'Y');
-        board.insert(2, 'R');
-        board.insert(3, 'Y');
-        board.insert(3, 'Y');
-        board.insert(3, 'Y');
-        board.insert(3, 'R');
-        assertEquals('R', board.calculateWinner()); // Should return 'R'
-
-        // Reset the board
-        board = new ConnectBoard(7, 6);
-
-        // Test diagonal win (top-left to bottom-right)
-        board.insert(3, 'Y');
-        board.insert(2, 'Y');
-        board.insert(1, 'Y');
-        board.insert(0, 'Y');
-        assertEquals('Y', board.calculateWinner()); // Should return 'Y'
+        assertEquals(Character.valueOf('X'), board.calculateWinner());
     }
 }
+
